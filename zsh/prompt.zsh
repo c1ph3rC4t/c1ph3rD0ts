7112fad9b95f756734efda450115ccc7ff369ce4
c1ph3rc4t <c1ph3rc4t@gmail.com>
2025-11-16 14:34:53 +0100

    Added Starship configs

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,4 +2,27 @@
 PROMPT="%n@%m:%~λ "
 
 # Run set up oh my posh if installed
-eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh.json 2>/dev/null || true)"
+if command -v oh-my-posh &>/dev/null; then
+    eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh.json 2>/dev/null || true)"
+fi
+
+# Starship setup
+if command -v starship &>/dev/null; then
+    set-long-prompt() {
+        eval "$(starship init zsh 2>/dev/null || true)"
+    }
+
+    precmd_functions+=(set-long-prompt)
+
+    set-short-prompt() {
+      if [[ $PROMPT != "%F{#8caaee}%f$(starship module directory)%F{#8caaee}─%f$(starship module character)" ]]; then
+        PROMPT="%F{#8caaee}%f$(starship module directory)%F{#8caaee}─%f$(starship module character)"
+        zle .reset-prompt
+      fi
+    }
+
+    zle-line-finish() { set-short-prompt }
+    zle -N zle-line-finish
+
+    trap 'set-short-prompt; return 130' INT
+fi
