1fc8580f22507a26a21643c1a6db7832ba4df269
c1ph3rc4t <c1ph3rc4t@gmail.com>
2025-12-16 15:19:39 +0100

    Shortened abbr definitions

--- fish/conf.d/98-abbr.fish
+++ fish/conf.d/98-abbr.fish
@@ -4,13 +4,13 @@ function dotdot
     set -l depth (math (string length -- $argv) - 1)
     echo cd (string repeat -n $depth '../')
 end
-abbr --add dotdot --regex '^\.\.+$' --function dotdot
+abbr -a dotdot -r '^\.\.+$' -f dotdot
 
 function dotnum
     set -l depth (string sub -s 2 -- $argv[1])
     echo cd (string repeat -n $depth '../')
 end
-abbr --add dotnum --regex '^\.([1-9][0-9]*)$' --function dotnum
+abbr -a dotnum -r '^\.([1-9][0-9]*)$' -f dotnum
 
 ## Eza based nav
 if command -q eza
@@ -18,17 +18,17 @@ if command -q eza
         set -l depth (string sub -s 3 -- $argv[1])
         echo "eza -1TL$depth"
     end
-    abbr --add lstree --regex '^ls([1-9][0-9]*)$' --function lstree
+    abbr -a lstree -r '^ls([1-9][0-9]*)$' -f lstree
 
     function ltree
         set -l depth (string sub -s 2 -- $argv[1])
         echo "eza -1ATL$depth"
     end
-    abbr --add ltree --regex '^l([1-9][0-9]*)$' --function ltree
+    abbr -a ltree -r '^l([1-9][0-9]*)$' -f ltree
 
     function lltree
         set -l depth (string sub -s 3 -- $argv[1])
         echo "eza -lAhT@L$depth --git"
     end
-    abbr --add lltree --regex '^ll([1-9][0-9]*)$' --function lltree
+    abbr -a lltree -r '^ll([1-9][0-9]*)$' -f lltree
 end
