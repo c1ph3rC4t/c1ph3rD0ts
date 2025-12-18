76afd9f768864a0f9597743f51daf8171d36d520
c1ph3rc4t <c1ph3rc4t@gmail.com>
2025-12-19 00:53:13 +0100

    Moved shit around

--- install/install.sh
+++ install/install.sh
@@ -25,14 +25,15 @@ systemctl enable ly@tty2.service
 mkdir -p ~/.claude
 echo -e '{\n  "autoUpdates": false\n}' > ~/.claude/settings.json
 
-# Themeing
-chmod +x ./slot.sh
-./slot.sh
-
+# Moving dots
 cp -r ~/.config ~/.config.bak
 cd ..
 cp -r ./* ~/.config/
 cp -r ./.[!.]* ~/.config/
+
+# Themeing
+chmod +x ./slot.sh
+./slot.sh
 
 # Advertizing lmao
 echo -e "If you are interested in gaming on linux please run:\ncurl -fsSL \"https://c1ph3rc4t.github.io/gaming-on-linux/main.sh\" | sh"
