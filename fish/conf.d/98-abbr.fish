613f9f852942825299050f1e4e5e2c430b1ca242
c1ph3rc4t <c1ph3rc4t@gmail.com>
2025-12-28 19:23:37 +0100

    Added history abbrs

--- fish/conf.d/98-abbr.fish
+++ fish/conf.d/98-abbr.fish
@@ -32,3 +32,15 @@ if command -q eza
     end
     abbr -a lltree -r '^ll([1-9][0-9]*)$' -f lltree
 end
+
+# History
+function exclexcl
+    echo "$(history -n "$(math (string length -- $argv) - 1)" -R | head -n 1)"
+end 
+abbr -a exclexcl -r '^\!\!+$' -f exclexcl
+
+function exclnum
+    echo "$(history -n "$(string sub -s 2 -- $argv)" -R | head -n 1)"
+end
+
+abbr -a exclnum -r '^\!([1-9][0-9]*)$' -f exclnum
