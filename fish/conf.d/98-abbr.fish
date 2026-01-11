0a140c44781ca34858866605fdff4d909ffc3395
c1ph3rc4t <c1ph3rc4t@gmail.com>
2026-01-11 01:06:45 +0100

    Added abbrs

--- fish/conf.d/98-abbr.fish
+++ fish/conf.d/98-abbr.fish
@@ -1,51 +1,65 @@
 # Navigation
 ## Cd based nav
-function dotdot
+function dotdot_abbr
     set -l depth (math (string length -- $argv) - 1)
     echo cd (string repeat -n $depth '../')
 end
-abbr -a dotdot -r '^\.\.+$' -f dotdot
+abbr -a dotdot -r '^\.\.+$' -f dotdot_abbr
 
-function dotnum
+function dotnum_abbr
     set -l depth (string sub -s 2 -- $argv[1])
     echo cd (string repeat -n $depth '../')
 end
-abbr -a dotnum -r '^\.([1-9][0-9]*)$' -f dotnum
+abbr -a dotnum -r '^\.([1-9][0-9]*)$' -f dotnum_abbr
 
 ## Eza based nav
 if command -q eza
-    function lstree
+    function lstree_abbr
         set -l depth (string sub -s 3 -- $argv[1])
         echo "eza -1TL$depth"
     end
-    abbr -a lstree -r '^ls([1-9][0-9]*)$' -f lstree
+    abbr -a lstree -r '^ls([1-9][0-9]*)$' -f lstree_abbr
 
-    function ltree
+    function ltree_abbr
         set -l depth (string sub -s 2 -- $argv[1])
         echo "eza -1ATL$depth"
     end
-    abbr -a ltree -r '^l([1-9][0-9]*)$' -f ltree
+    abbr -a ltree -r '^l([1-9][0-9]*)$' -f ltree_abbr
 
-    function lltree
+    function lltree_abbr
         set -l depth (string sub -s 3 -- $argv[1])
         echo "eza -lAhT@L$depth --git"
     end
-    abbr -a lltree -r '^ll([1-9][0-9]*)$' -f lltree
+    abbr -a lltree -r '^ll([1-9][0-9]*)$' -f lltree_abbr
 end
 
-# History
-function exclexcl
-    echo "$(history -n "$(math (string length -- $argv) - 1)" -R | head -n 1)"
-end
-abbr -a exclexcl -r '^\!\!+$' -f exclexcl
-
-function exclnum
-    echo "$(history -n "$(string sub -s 2 -- $argv)" -R | head -n 1)"
-end
-abbr -a exclnum -r '^\!([1-9][0-9]*)$' -f exclnum
-
-# Unalias
-function exclcmd
+# Dealias
+function dealias_abbr
     echo "command $(string sub -s 2 -- $argv)"
 end
-abbr -a exclcmd -r '^!\w+$' -f exclcmd
+abbr -a dealias -r '^!.+$' -f dealias_abbr
+
+# History
+function exclexcl_abbr
+    echo "$(history -n "$(math (string length -- $argv) - 1)" -R | head -n 1)"
+end
+abbr -a exclexcl -r '^\!\!+$' -f exclexcl_abbr
+
+function exclnum_abbr
+    echo "$(history -n "$(string sub -s 2 -- $argv)" -R | head -n 1)"
+end
+abbr -a exclnum -r '^\!([1-9][0-9]*)$' -f exclnum_abbr
+
+# Sudo
+function sudoify_abbr
+    echo "sudo $(string sub -s 2 -- $argv)"
+end
+abbr -a sudoify -r '^§.+$' -f sudoify_abbr
+
+# AUR helper
+if test "$AUR_HELPER" != ''
+    function aur_abbr
+        echo "$AUR_HELPER -$(string sub -s 2 -- $argv)"
+    end
+    abbr -a aur -r '^y[A-Z]\w*$' -f aur_abbr
+end
