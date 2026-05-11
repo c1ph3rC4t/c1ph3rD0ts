f745d43e925b9374befb94220bed22a5013769da
c1ph3rc4t <c1ph3rc4t@gmail.com>
2026-05-11 11:35:33 +0200

    Dump update + new dump

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -2,3 +2,4 @@
 # Dump only explicitly installed, top-level packages (not pulled in as deps)
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 yay -Qqet > "$SCRIPT_DIR/data/aur-deps"
+code --list-extensions > "$SCRIPT_DIR/data/vscode-extensions"
