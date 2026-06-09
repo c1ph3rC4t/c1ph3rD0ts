4012064ee7df997e6c4babe11f9485a2200345e1
c1ph3rc4t <c1ph3rc4t@gmail.com>
2026-06-09 20:15:10 +0200

    Fix

--- hypr/hyprland.d/01-env.lua
+++ hypr/hyprland.d/01-env.lua
@@ -45,7 +45,7 @@ hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
-hl.env("XDG_MENU_PREFIX", "arch-")
+hl.env("XDG_MENU_PREFIX", "plasma-")
 
 hl.env("GDK_BACKEND", "wayland,x11")
 hl.env("GTK_THEME", "catppuccin-frappe-sapphire-standard+default")
